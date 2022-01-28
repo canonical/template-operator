77a9fc5f08b5b84bc6ef4fecef13b8b6c04065e7
Andrew Scribner <ca.scribner+1@gmail.com>
2022-01-28 14:59:37 -0500

    fix formatting
    human linting fail...

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -31,7 +31,12 @@ async def test_build_and_deploy(ops_test: OpsTest):
     # issuing dummy update_status just to trigger an event
     await ops_test.model.set_config({"update-status-hook-interval": "10s"})
 
-    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000)
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME], 
+        status="active", 
+        raise_on_blocked=True, 
+        timeout=1000
+    )
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
     # effectively disable the update status from firing
