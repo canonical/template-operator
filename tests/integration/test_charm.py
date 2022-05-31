c1786b5c3064b46d0b26b439b4f8fa2319a363b1
Pietro Pasotti <starfire.daemon@gmail.com>
2022-05-31 15:57:42 +0200

    added ffwd util

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -29,18 +29,14 @@ async def test_build_and_deploy(ops_test: OpsTest):
     await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
 
     # issuing dummy update_status just to trigger an event
-    await ops_test.model.set_config({"update-status-hook-interval": "10s"})
-
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME],
-        status="active",
-        raise_on_blocked=True,
-        timeout=1000,
-    )
-    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
-
-    # effectively disable the update status from firing
-    await ops_test.model.set_config({"update-status-hook-interval": "60m"})
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="active",
+            raise_on_blocked=True,
+            timeout=1000,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
 
 @pytest.mark.abort_on_fail
